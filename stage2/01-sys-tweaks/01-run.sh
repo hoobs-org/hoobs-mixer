3a92e157446c78807a21aa543e387e75adf80ee4
Michael Kellsy <mkellsy@gmail.com>
2020-04-15 11:20:06 -0600

    initial commit

---
+++ stage2/01-sys-tweaks/01-run.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -e
+
+##################################################################################################
+# rpi-gen                                                                                        #
+# Copyright (C) 2015 Raspberry Pi (Trading) Ltd.                                                 #
+#                                                                                                #
+# This program is free software: you can redistribute it and/or modify                           #
+# it under the terms of the GNU General Public License as published by                           #
+# the Free Software Foundation, either version 3 of the License, or                              #
+# (at your option) any later version.                                                            #
+#                                                                                                #
+# This program is distributed in the hope that it will be useful,                                #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                                 #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  #
+# GNU General Public License for more details.                                                   #
+#                                                                                                #
+# You should have received a copy of the GNU General Public License                              #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.                          #
+##################################################################################################
+
+install -m 755 files/resize2fs_once	"${ROOTFS_DIR}/etc/init.d/"
+
+install -d				"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d"
+install -m 644 files/ttyoutput.conf	"${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/"
+
+install -m 644 files/50raspi		"${ROOTFS_DIR}/etc/apt/apt.conf.d/"
+
+install -m 644 files/console-setup   	"${ROOTFS_DIR}/etc/default/"
+
+install -m 755 files/rc.local		"${ROOTFS_DIR}/etc/"
+
+on_chroot << EOF
+systemctl disable hwclock.sh
+systemctl disable nfs-common
+systemctl disable rpcbind
+if [ "${ENABLE_SSH}" == "1" ]; then
+	systemctl enable ssh
+else
+	systemctl disable ssh
+fi
+systemctl enable regenerate_ssh_host_keys
+EOF
+
+if [ "${USE_QEMU}" = "1" ]; then
+	echo "enter QEMU mode"
+	install -m 644 files/90-qemu.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
+	on_chroot << EOF
+systemctl disable resize2fs_once
+EOF
+	echo "leaving QEMU mode"
+else
+	on_chroot << EOF
+systemctl enable resize2fs_once
+EOF
+fi
+
+on_chroot <<EOF
+for GRP in input spi i2c gpio; do
+	groupadd -f -r "\$GRP"
+done
+for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev; do
+  adduser $FIRST_USER_NAME \$GRP
+done
+EOF
+
+on_chroot << EOF
+setupcon --force --save-only -v
+EOF
+
+on_chroot << EOF
+usermod --pass='*' root
+EOF
+
+rm -f "${ROOTFS_DIR}/etc/ssh/"ssh_host_*_key*
